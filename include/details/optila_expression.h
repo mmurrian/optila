0555d14407c7924426407668c44d8cd0c49a2eee
Matthew Murrian <matthewmurrian@gmail.com>
2023-04-17 12:06:39 -0400

    A little clean-up

--- include/details/optila_expression.h
+++ include/details/optila_expression.h
@@ -29,65 +29,6 @@ template <std::size_t Index, typename Expr>
 inline constexpr bool is_operand_expression_literal_v =
     is_operand_expression_literal<Index, Expr>::value;
 
-template <typename T>
-struct is_matrix_or_scalar_literal
-    : std::integral_constant<bool, is_matrix_literal_v<T> ||
-                                       is_scalar_literal_v<T>> {};
-
-template <typename T>
-inline constexpr bool is_matrix_or_scalar_literal_v =
-    is_matrix_or_scalar_literal<T>::value;
-
-template <typename T>
-struct is_expression : std::disjunction<is_matrix<T>, is_scalar<T>> {};
-
-template <typename T>
-inline constexpr bool is_expression_v = is_expression<T>::value;
-
-template <typename Lhs, typename Rhs>
-struct is_unary_operand_pair
-    : std::conjunction<is_expression<Lhs>, std::is_void<Rhs>> {};
-
-template <typename Lhs, typename Rhs>
-inline constexpr bool is_unary_operand_pair_v =
-    is_unary_operand_pair<Lhs, Rhs>::value;
-
-template <typename Lhs, typename Rhs>
-struct is_binary_operand_pair
-    : std::conjunction<is_expression<Lhs>, is_expression<Rhs>> {};
-
-template <typename Lhs, typename Rhs>
-inline constexpr bool is_binary_operand_pair_v =
-    is_binary_operand_pair<Lhs, Rhs>::value;
-
-template <typename Lhs, typename Rhs>
-struct is_either_scalar : std::disjunction<is_scalar<Lhs>, is_scalar<Rhs>> {};
-
-template <typename Lhs, typename Rhs>
-inline constexpr bool is_either_scalar_v = is_either_scalar<Lhs, Rhs>::value;
-
-template <typename Lhs, typename Rhs>
-struct is_either_matrix : std::disjunction<is_matrix<Lhs>, is_matrix<Rhs>> {};
-
-template <typename Lhs, typename Rhs>
-inline constexpr bool is_either_matrix_v = is_either_matrix<Lhs, Rhs>::value;
-
-template <typename Lhs, typename Rhs>
-struct is_binary_matrix_pair
-    : std::conjunction<is_matrix<Lhs>, is_matrix<Rhs>> {};
-
-template <typename Lhs, typename Rhs>
-inline constexpr bool is_binary_matrix_pair_v =
-    is_binary_matrix_pair<Lhs, Rhs>::value;
-
-template <typename Lhs, typename Rhs>
-struct is_binary_scalar_pair
-    : std::conjunction<is_scalar<Lhs>, is_scalar<Rhs>> {};
-
-template <typename Lhs, typename Rhs>
-inline constexpr bool is_binary_scalar_pair_v =
-    is_binary_scalar_pair<Lhs, Rhs>::value;
-
 template <typename Lhs>
 struct is_static_expression
     : std::conditional_t<std::decay_t<Lhs>::num_rows_static() != Dynamic &&
--- include/details/optila_matrix.h
+++ include/details/optila_matrix.h
@@ -2,6 +2,7 @@
 
 #include <array>
 #include <cstddef>
+#include <type_traits>
 
 namespace optila {
 
@@ -64,12 +65,15 @@ struct is_matrix_literal<Matrix<ValueType, NumRows, NumCols, Order>>
 template <typename T>
 inline constexpr bool is_matrix_literal_v = is_matrix_literal<T>::value;
 
-template <typename ValueType, std::size_t NumRows, std::size_t NumCols>
+template <typename ValueType, std::size_t NumRows, std::size_t NumCols,
+          bool OwnsData = true>
 struct MatrixStaticStorage {
   using value_type = ValueType;
 
   constexpr value_type* data() { return data_.data(); }
-  constexpr const value_type* data() const { return data_.data(); }
+  [[nodiscard]] constexpr const value_type* data() const {
+    return data_.data();
+  }
 
   constexpr value_type& operator[](std::size_t idx) { return data_[idx]; }
   constexpr const value_type& operator[](std::size_t idx) const {
@@ -82,24 +86,39 @@ struct MatrixStaticStorage {
   static constexpr std::size_t size() { return NumRows * NumCols; }
 
  private:
-  std::array<value_type, NumRows * NumCols> data_{};
+  struct array_wrapper {
+    constexpr value_type& operator[](std::size_t idx) { return data_[idx]; }
+    constexpr const value_type& operator[](std::size_t idx) const {
+      return data_[idx];
+    }
+
+    constexpr value_type* data() { return data_; }
+    [[nodiscard]] constexpr const value_type* data() const { return data_; }
+
+   private:
+    value_type* data_{};
+  };
+
+  using storage_type =
+      std::conditional_t<OwnsData, std::array<value_type, NumRows * NumCols>,
+                         array_wrapper>;
+  storage_type data_{};
 };
 
-template <typename ValueType>
+template <typename ValueType, bool OwnsData = true>
 struct MatrixDynamicStorage {
   using value_type = ValueType;
 
   MatrixDynamicStorage() = default;
   ~MatrixDynamicStorage() {
-    if (owns_data_) delete[] data_;
+    if constexpr (OwnsData) delete[] data_;
   }
   MatrixDynamicStorage(const MatrixDynamicStorage& from) { *this = from; }
   MatrixDynamicStorage& operator=(const MatrixDynamicStorage& from) {
     if (this != &from) {
       num_rows_ = from.num_rows_;
       num_cols_ = from.num_cols_;
-      owns_data_ = from.owns_data_;
-      if (from.owns_data_) {
+      if constexpr (OwnsData) {
         data_ = new value_type[num_rows_ * num_cols_];
         std::copy(from.data_, from.data_ + num_rows_ * num_cols_, data_);
       } else {
@@ -115,12 +134,11 @@ struct MatrixDynamicStorage {
     std::swap(num_rows_, from.num_rows_);
     std::swap(num_cols_, from.num_cols_);
     std::swap(data_, from.data_);
-    std::swap(owns_data_, from.owns_data_);
     return *this;
   }
 
   constexpr value_type* data() { return data_; }
-  constexpr const value_type* data() const { return data_; }
+  [[nodiscard]] constexpr const value_type* data() const { return data_; }
 
   constexpr value_type& operator[](std::size_t idx) { return data_[idx]; }
   constexpr const value_type& operator[](std::size_t idx) const {
@@ -137,17 +155,29 @@ struct MatrixDynamicStorage {
   void resize(std::size_t num_rows, std::size_t num_cols) {
     num_rows_ = num_rows;
     num_cols_ = num_cols;
-    if (owns_data_) delete[] data_;
-    data_ = new value_type[num_rows * num_cols];
-    owns_data_ = true;
+    if (OwnsData) {
+      delete[] data_;
+      data_ = new value_type[num_rows * num_cols];
+    }
   }
 
  private:
   std::size_t num_rows_{};
   std::size_t num_cols_{};
   value_type* data_{};
-  bool owns_data_{false};
 };
+
+// A non-owning, static storage matrix is just a pointer to the data
+static_assert(sizeof(MatrixStaticStorage<double, 10, 10, false>) <=
+              std::max(alignof(double*), sizeof(double*)));
+
+// An owning, static storage matrix is just an array
+static_assert(sizeof(MatrixStaticStorage<double, 10, 10, true>) <=
+              std::max(alignof(double), sizeof(double)) * 10 * 10);
+
+// Owning and non-owning dynamic storage matrices are the same size
+static_assert(sizeof(MatrixDynamicStorage<double, false>) ==
+              sizeof(MatrixDynamicStorage<double, true>));
 
 }  // namespace details
 }  // namespace optila
